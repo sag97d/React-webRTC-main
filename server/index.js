6a165cd3c2c3e38b4f18e9ee2704952fa7ae4c12
sag97d <131565578+sag97d@users.noreply.github.com>
2025-04-06 03:06:57 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const { Server } = require("socket.io");
 
 const io = new Server(8000, {
-  cors: true,
+   origin: "https://your-frontend-url.com",
+    methods: ["GET", "POST"]
 });
 
 const emailToSocketIdMap = new Map();
